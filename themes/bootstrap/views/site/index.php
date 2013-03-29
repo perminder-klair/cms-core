5f46c3dba68e256650df2bcb157a34e485eaea45
Parminder Klair <parminder@tblmarketing.com>
2013-03-29 18:11:00 +0000

    updated bootstrap home page with some docs links

--- themes/bootstrap/views/site/index.php
+++ themes/bootstrap/views/site/index.php
@@ -1,11 +1,15 @@
 <?php
-/* @var $this SiteController */
-
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle='Home';
+//$this->pageDescription='';
+//$this->pageKeywords='';
+//$this->pageAuthor='';
+$this->pageOgTitle = 'my title';
+$this->pageOgDesc = 'my title';
+$this->pageOgImage = 'my title';
 ?>
 
 <?php $this->beginWidget('bootstrap.widgets.TbHeroUnit',array(
-    'heading'=>'Welcome to '.CHtml::encode(Yii::app()->name),
+    'heading'=>'Welcome to '.gl('site_name'),
 )); ?>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
@@ -23,3 +27,5 @@ $this->pageTitle=Yii::app()->name;
     the <a href="http://www.yiiframework.com/doc/">documentation</a>.
     Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
     should you have any questions.</p>
+    
+<p>For bootstrap docs read <a href="http://www.cniska.net/yii-bootstrap/">Yii-Bootstrap Docs</a> and official bootstrap <a href="http://twitter.github.com/bootstrap/">documentation</a>, also read here for more <a href="http://yii-booster.clevertech.biz/index.html">resources</a></p>
