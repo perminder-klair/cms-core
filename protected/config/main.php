2d5432c095366012936ed44f303fe0acb36566a2
Parminder Klair <parminder@tblmarketing.com>
2013-03-31 16:43:43 +0100

    updated gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -125,6 +125,6 @@ return array(
 	'params'=>array(
 		// this is used in contact page
 		//'adminEmail'=>'webmaster@example.com',
-		'adminMenu'=>array('properties'),
+		'adminMenu'=>array(),
 	),
 );
